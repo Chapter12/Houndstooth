13958af6591a4fbc1455f5aa4de37fd41a37dd0c
Daniel Cartwright <dan@houndworld.org>
2014-10-14 17:22:18 +0100

    Show post titles automatically.

--- wp-content/themes/houndstooth/content.php
+++ wp-content/themes/houndstooth/content.php
@@ -1,3 +1,10 @@
+<!-- content.php -->
+
+<header class="entry-header">
+	<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+
+	</header><!-- .entry-header -->
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 	<?php if (is_search()) { // Only display Excerpts for Search ?> 
